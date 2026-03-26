96f3157b3443ed0227c885ee13977ab43ede5312
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2026-03-25 21:11:30 -0300

    upload logo google

--- frontend/src/app/components/organisms/DetailsPlayer.jsx
+++ frontend/src/app/components/organisms/DetailsPlayer.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import PlayerInfo from '../molecules/PlayerInfo';
 import PlayerStats from '../molecules/PlayerStats';
 import PlayerTrophie from '../molecules/PlayerTrophie';
